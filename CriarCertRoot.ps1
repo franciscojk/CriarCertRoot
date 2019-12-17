13265edc4123242e01f80de0bc6e9876e44d651e
Francisco Kraetzer <francisco@multitask.com.br>
2019-12-17 13:47:15 -0300

    ok

--- CriarCertRoot.ps1
+++ CriarCertRoot.ps1
@@ -1,3 +1,13 @@
 ﻿#
 # Script.ps1
 #
+$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+	-Subject "CN=VPN-Server" -KeyExportPolicy Exportable -HashAlgorithm sha256 `
+	-KeyLength 2048 -CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign `
+	-KeyUsage CertSign
+
+
+New-SelfSignedCertificate -Type Custom -DnsName vpn -KeySpec Signature `
+	-Subject "VPN-Client" -KeyExportPolicy Exportable  -HashAlgorithm sha256 `
+	-KeyLength 2048 -CertStoreLocation "Cert:\CurrentUser\My" `
+	-Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
